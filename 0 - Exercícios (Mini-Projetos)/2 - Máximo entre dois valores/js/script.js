dccc40d7da13f765fea9de91b92dca2be3651f71
Bruno Eduardo <brumo1234@gmail.com>
2021-09-06 22:16:22 -0300

    Exercise 1 add

---
+++ 0 - Exercícios (Mini-Projetos)/2 - Máximo entre dois valores/js/script.js
@@ -0,0 +1,17 @@
+let valorMaior = max(-1,18);
+console.log(valorMaior);
+
+function max(n1,n2){
+    if(n1>n2)
+        return n1;
+    else return n2;
+}
+
+//
+
+let valorMaior2 = max(6,2);
+console.log(valorMaior2);
+
+function max(n1,n2){
+    return n1>n2 ? n1 : n2;
+}
---
+++ 9 - Operadores/8 - Laços/5 - For..of/js/script.js
@@ -0,0 +1,17 @@
+// No JS temos 5 laços:
+
+// For
+// While
+// Do..while
+// For..in
+// For..of
+
+//
+
+//for..of
+
+const cores = ['Vermelho','Azul','Verde'];
+
+ for(let cor of cores){
+    console.log(cor);
+ }
